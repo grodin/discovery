553a1ddf599102311f7e859eb991570e005bc105
Joseph Cooper <cooper.joseph@gmail.com>
2024-11-08 18:27:18 +0000

    Finish chapters up to 7

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,39 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use microbit::display::blocking::Display;
+use microbit::hal::timer::Timer;
+use microbit::Board;
+use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let (mut row, mut col) = (0, 0);
+
+    let mut leds = [[0; 5]; 5];
+
+    loop {
+        leds[row][col] = 0;
+        (row, col) = compute_next_row_and_col(row, col);
+        leds[row][col] = 1;
+        display.show(&mut timer, leds, 300);
+    }
+}
+
+fn compute_next_row_and_col(row: usize, col: usize) -> (usize, usize) {
+    assert!(row < 5 && col < 5);
+
+    match (row, col) {
+        (0, col) if col < 4 => (0, col + 1),
+        (row, 4) if row < 4 => (row + 1, 4),
+        (4, col) if col > 0 => (4, col - 1),
+        (row, 0) if row > 0 => (row - 1, 0),
+        _ => panic!("Already passed the assertion, how did we get here???"),
+    }
 }
--- microbit/src/07-uart/src/main.rs
+++ microbit/src/07-uart/src/main.rs
@@ -2,26 +2,18 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use rtt_target::rtt_init_print;
 use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+use core::fmt::Write;
 
-#[cfg(feature = "v1")]
-use microbit::{
-    hal::prelude::*,
-    hal::uart,
-    hal::uart::{Baudrate, Parity},
-};
 
-#[cfg(feature = "v2")]
 use microbit::{
     hal::prelude::*,
     hal::uarte,
     hal::uarte::{Baudrate, Parity},
 };
 
-#[cfg(feature = "v2")]
 mod serial_setup;
-#[cfg(feature = "v2")]
 use serial_setup::UartePort;
 
 #[entry]
@@ -29,17 +21,7 @@ fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
-    #[cfg(feature = "v1")]
-    let mut serial = {
-        uart::Uart::new(
-            board.UART0,
-            board.uart.into(),
-            Parity::EXCLUDED,
-            Baudrate::BAUD115200,
-        )
-    };
 
-    #[cfg(feature = "v2")]
     let mut serial = {
         let serial = uarte::Uarte::new(
             board.UARTE0,
@@ -50,8 +32,28 @@ fn main() -> ! {
         UartePort::new(serial)
     };
 
-    nb::block!(serial.write(b'X')).unwrap();
-    nb::block!(serial.flush()).unwrap();
 
-    loop {}
+    let mut input_buffer = heapless::Vec::<u8, 32>::new();
+    loop {
+        input_buffer.clear();
+        let mut byte = 0_u8;
+
+        let success = loop {
+            byte = nb::block!(serial.read()).unwrap();
+
+            if byte == b'\r' {
+                break true;
+            }
+
+            if input_buffer.push(byte).is_err() {
+                write!(serial, "Error: input buffer is full!\r\n").unwrap();
+                break false;
+            }
+        };
+        if success {
+            input_buffer.reverse();
+            write!(serial, "{}\r\n", core::str::from_utf8(&input_buffer).unwrap()).unwrap();
+        }
+        nb::block!(serial.flush()).unwrap();
+    }
 }
